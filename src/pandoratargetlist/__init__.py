daa73ca35de6143ca3ec63a99601065377a01feb
benhord <benjhord@gmail.com>
2025-04-29 15:03:31 -0400

    Included PSF load as standard for import

--- src/pandoratargetlist/__init__.py
+++ src/pandoratargetlist/__init__.py
@@ -2,8 +2,13 @@ __version__ = "0.1.3"
 # Standard library
 import os  # noqa
 
+import pandorapsf as ppsf # noqa
+
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 HOMEDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/"
 TARGDEFDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + '/target_definition_files/'
 
-from .json_targets import make_json_file  # noqa
+VDA_PSF = ppsf.PSF.from_name("VISDA")
+NIRDA_PSF = ppsf.PSF.from_name("NIRDA")
+
+from .json_targets import make_target_file  # noqa
