98b3a3b88106ce19e18d8e02615b05ed6efc90c6
benhord <benjhord@gmail.com>
2025-04-30 14:31:48 -0400

    Formatting

--- src/pandoratargetlist/__init__.py
+++ src/pandoratargetlist/__init__.py
@@ -2,11 +2,11 @@ __version__ = "0.1.3"
 # Standard library
 import os  # noqa
 
-import pandorapsf as ppsf # noqa
+import pandorapsf as ppsf  # noqa
 
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 HOMEDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/"
-TARGDEFDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + '/target_definition_files/'
+TARGDEFDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/target_definition_files/"
 
 VDA_PSF = ppsf.PSF.from_name("VISDA")
 NIRDA_PSF = ppsf.PSF.from_name("NIRDA")
