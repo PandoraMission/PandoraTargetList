47a9af14b8260ce8bb4234ae81f7fb30ea0c05b8
benhord <benjhord@gmail.com>
2025-05-06 14:33:34 -0400

    Removing default PSF imports

--- src/pandoratargetlist/__init__.py
+++ src/pandoratargetlist/__init__.py
@@ -8,7 +8,4 @@ PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 HOMEDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/"
 TARGDEFDIR = "/".join(PACKAGEDIR.split("/")[:-2]) + "/target_definition_files/"
 
-VDA_PSF = ppsf.PSF.from_name("VISDA")
-NIRDA_PSF = ppsf.PSF.from_name("NIRDA")
-
 from .targets import make_target_file  # noqa
